b040e468f5df972f981b3500536b1ac0f1babf1e
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2017-11-24 10:35:59 -0700

    Refactored User endpoint and added Group endpoint.
    This commit refactors some of the code that was used to define
    the User endpoint and adds a new Group endpoint to manage user
    groups.

--- airu_api/urls.py
+++ airu_api/urls.py
@@ -1,5 +1,4 @@
-"""airu_api URL Configuration
-
+"""airu_api URL Configuration 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
@@ -15,23 +14,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from rest_framework import routers
+from airu_api import views
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
